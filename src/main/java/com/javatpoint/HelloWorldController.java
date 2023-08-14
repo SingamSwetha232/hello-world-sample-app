b7d4709ac10ab026389d1048ac556b8dde0be236
Singam Swetha Reddy <swethareddy2322570@gmail.com>
2023-08-15 00:20:44 +0530

    test

--- src/main/java/com/javatpoint/HelloWorldController.java
+++ src/main/java/com/javatpoint/HelloWorldController.java
@@ -1,4 +1,4 @@
-package com.javatpoint.controller;
+package com.javatpoint;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 @RestController
